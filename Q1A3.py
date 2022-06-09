0584a62168fdf01050e43d5ee990e894478ab4fb
KushagraSingal <104300046+KushagraSingal@users.noreply.github.com>
2022-06-09 13:45:27 +0530

    Add files via upload

---
+++ Q1A3.py
@@ -0,0 +1,7 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a= int(input("Enter number you want to convert to binary :"))
+Enter number you want to convert to binary :85
+b= bin(a)
+print (b)
+0b1010101
